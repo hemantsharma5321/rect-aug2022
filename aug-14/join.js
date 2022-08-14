fb0999eae445d573587a3cd34bd3123c19c5dfa1
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2022-08-14 12:37:32 +0530

    Create join.js

---
+++ aug-14/join.js
@@ -0,0 +1,10 @@
+let mycolor=["Red","Green","White","Black"];
+const joinarr=(mycolor)=>{
+
+
+let result=mycolor.join(',');
+let result2=mycolor.join('+');
+console.log(result);
+console.log(result2);
+}
+joinarr(mycolor);
